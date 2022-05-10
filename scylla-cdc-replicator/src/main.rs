27db5b1f1901641afe182146c6fcd56acf6e1303
Marcin Mazurek <marcinek.mazurek@gmail.com>
2022-05-10 15:00:48 +0200

    Make replicator executable

--- scylla-cdc-replicator/src/main.rs
+++ scylla-cdc-replicator/src/main.rs
@@ -1,29 +1,94 @@
 mod replication_tests;
+mod replicator;
 pub mod replicator_consumer;
 
-use replicator_consumer::ReplicatorConsumerFactory;
-use scylla::SessionBuilder;
-use std::error::Error;
-use std::sync::Arc;
 use std::time::Duration;
 
+use clap::Parser;
+use futures_util::StreamExt;
+use tokio::select;
+
+use crate::replicator::Replicator;
+
+#[derive(Parser)]
+struct Args {
+    /// Keyspace name
+    #[clap(short, long)]
+    keyspace: String,
+
+    /// Table names provided as a comma delimited string
+    #[clap(short, long)]
+    table: String,
+
+    /// Address of a node in source cluster
+    #[clap(short, long)]
+    source: String,
+
+    /// Address of a node in destination cluster
+    #[clap(short, long)]
+    destination: String,
+
+    /// Window size in seconds
+    #[clap(long, default_value_t = 60.)]
+    window_size: f64,
+
+    /// Safety interval in seconds
+    #[clap(long, default_value_t = 30.)]
+    safety_interval: f64,
+
+    /// Sleep interval in seconds
+    #[clap(long, default_value_t = 10.)]
+    sleep_interval: f64,
+
+    /// Start datetime as RFC 3339 formatted string
+    #[clap(long)]
+    start_datetime: Option<String>,
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello");
-    tokio::time::sleep(Duration::from_secs(1)).await;
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let tables: Vec<_> = args.table.split(',').map(|s| s.to_string()).collect();
+    assert!(!tables.is_empty(), "no tables were provided");
+    let start_timestamp = chrono::Duration::milliseconds(match args.start_datetime {
+        Some(s) => chrono::DateTime::parse_from_rfc3339(&s)?.timestamp_millis(),
+        None => chrono::Local::now().timestamp_millis(),
+    });
+    let sleep_interval = Duration::from_secs_f64(args.sleep_interval);
+    let mut result = Ok(());
 
-    // Create a factory to remove the warning about unused new() function.
-    let _factory = ReplicatorConsumerFactory::new(
-        Arc::new(
-            SessionBuilder::new()
-                .known_node("127.0.0.1:9042")
-                .build()
-                .await?,
-        ),
-        "ks".to_string(),
-        "t".to_string(),
-    );
+    let (mut replicator, mut handles) = Replicator::new(
+        args.source,
+        args.keyspace.clone(),
+        tables.clone(),
+        args.destination,
+        args.keyspace,
+        tables,
+        start_timestamp,
+        Duration::from_secs_f64(args.window_size),
+        Duration::from_secs_f64(args.safety_interval),
+        sleep_interval,
+    )
+    .await?;
 
-    println!("Replicator");
-    Ok(())
+    select! {
+        _ = tokio::signal::ctrl_c() => {
+            println!("Shutting down...");
+        }
+        res = handles.next() => {
+            // The only way this could have happened is an error in on of the CDCLogReaders so we
+            // stop all of them.
+            result = res.unwrap();
+            println!("{:?}", result);
+        }
+    }
+
+    replicator.stop();
+    while let Some(res) = handles.next().await {
+        if res.is_err() {
+            result = res;
+            println!("{:?}", result);
+        }
+    }
+    result
 }
---
+++ scylla-cdc-replicator/src/replicator.rs
@@ -0,0 +1,62 @@
+use std::sync::Arc;
+use std::time;
+
+use futures_util::future::RemoteHandle;
+use futures_util::stream::FuturesUnordered;
+use scylla::SessionBuilder;
+use scylla_cdc::log_reader::{CDCLogReader, CDCLogReaderBuilder};
+
+use crate::replicator_consumer::ReplicatorConsumerFactory;
+
+pub struct Replicator {
+    log_readers: Vec<CDCLogReader>,
+}
+
+impl Replicator {
+    #[allow(clippy::too_many_arguments)]
+    pub async fn new(
+        source_uri: String,
+        source_keyspace: String,
+        source_tables: Vec<String>,
+        dest_uri: String,
+        dest_keyspace: String,
+        dest_tables: Vec<String>,
+        start_timestamp: chrono::Duration,
+        window_size: time::Duration,
+        safety_interval: time::Duration,
+        sleep_interval: time::Duration,
+    ) -> anyhow::Result<(Self, FuturesUnordered<RemoteHandle<anyhow::Result<()>>>)> {
+        let source_session = Arc::new(SessionBuilder::new().known_node(source_uri).build().await?);
+        let dest_session = Arc::new(SessionBuilder::new().known_node(dest_uri).build().await?);
+        let mut log_readers = vec![];
+        let handles = FuturesUnordered::new();
+
+        for (source_table, dest_table) in source_tables.into_iter().zip(dest_tables) {
+            let factory = ReplicatorConsumerFactory::new(
+                dest_session.clone(),
+                dest_keyspace.clone(),
+                dest_table,
+            )?;
+            let (log_reader, handle) = CDCLogReaderBuilder::new()
+                .session(source_session.clone())
+                .keyspace(&source_keyspace)
+                .table_name(&source_table)
+                .start_timestamp(start_timestamp)
+                .window_size(window_size)
+                .safety_interval(safety_interval)
+                .sleep_interval(sleep_interval)
+                .consumer_factory(Arc::new(factory))
+                .build()
+                .await?;
+            log_readers.push(log_reader);
+            handles.push(handle);
+        }
+        Ok((Replicator { log_readers }, handles))
+    }
+
+    pub fn stop(&mut self) {
+        for log_reader in &mut self.log_readers {
+            log_reader.stop();
+        }
+    }
+}
