fd5b47ab29714dab4d4830689d6894b80cdbfe21
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2022-06-08 20:49:07 +0200

    Change modules visibility. Now only log_reader and consumer modules are public.

--- scylla-cdc/src/lib.rs
+++ scylla-cdc/src/lib.rs
@@ -1,9 +1,9 @@
-pub mod cdc_types;
+mod cdc_types;
 pub mod consumer;
 mod e2e_tests;
 pub mod log_reader;
-pub mod stream_generations;
-pub mod stream_reader;
+mod stream_generations;
+mod stream_reader;
 
 // The test module should be visible only if test feature is enabled.
 // Test feature is disabled by default.
